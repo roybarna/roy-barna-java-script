365823c849af18eb725fd7d08e866dc8f2f9dafa
roybarna <117443960+roybarna@users.noreply.github.com>
2023-03-24 15:45:16 +0530

    Add files via upload

---
+++ comparision oprator.js
@@ -0,0 +1,37 @@
+1. <html>
+2. <body> 
+3. <script type = "text/javascript">
+4. <!--
+5. var a = 10;
+6. var b = 20;
+7. var linebreak = "<br />";
+8. document.write("(a == b) => ");
+9. result = (a == b);
+10. document.write(result);
+11. document.write(linebreak);
+12. document.write("(a < b) => ");
+13. result = (a < b);
+14. document.write(result);
+15. document.write(linebreak);
+16. document.write("(a > b) => ");
+17. result = (a > b);
+18. document.write(result);
+19. document.write(linebreak);
+20. document.write("(a != b) => ");
+21. result = (a != b);
+22. document.write(result);
+23. document.write(linebreak);
+24. document.write("(a >= b) => ");
+25. result = (a >= b);
+26. document.write(result);
+27. document.write(linebreak);
+28. document.write("(a <= b) => ");
+29. result = (a <= b);
+30. document.write(result);
+31. document.write(linebreak);
+32. //-->
+33. </script> 
+34. Set the variables to different values and different operators and then try...
+35. </body>
+36. </html>
+
