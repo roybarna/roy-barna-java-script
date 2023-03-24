1f988b5947e3bc70b9f25147389e6c2f53f8a36a
roybarna <117443960+roybarna@users.noreply.github.com>
2023-03-24 15:42:50 +0530

    Add files via upload

---
+++ addition opretor.js
@@ -0,0 +1,43 @@
+1. <html>
+2. <body>
+3. <script type = "text/javascript">
+4. <!--
+5. var a = 33;
+6. var b = 10;
+7. var c = "Test";
+8. var linebreak = "<br />";
+9. document.write("a + b = ");
+10. result = a + b;
+11. document.write(result);
+12. document.write(linebreak);
+13. document.write("a - b = ");
+14. result = a - b;
+15. document.write(result);
+16. document.write(linebreak);
+17. document.write("a / b = ");
+18. result = a / b;
+19. document.write(result);
+20. document.write(linebreak);
+21. document.write("a % b = ");
+22. result = a % b;
+23. document.write(result);
+24. document.write(linebreak);
+25. document.write("a + b + c = ");
+26. result = a + b + c;
+27. document.write(result);
+28. document.write(linebreak);
+29. a = ++a;
+30. document.write("++a = ");
+31. result = ++a;
+32. document.write(result);
+33. document.write(linebreak);
+34. b = --b;
+35. document.write("--b = ");
+36. result = --b;
+37. document.write(result);
+38. document.write(linebreak);
+39. //-->
+40. </script>
+41. Set the variables to different values and then try...
+42. </body>
+43. </html>
