70791ac914178fcb060c7af1fe9e22c6c29d604a
roybarna <117443960+roybarna@users.noreply.github.com>
2023-03-24 15:40:20 +0530

    Add files via upload

---
+++ local variabls.js
@@ -0,0 +1,11 @@
+1. <html>
+2. <body onload = checkscope();> 
+3. <script type = "text/javascript">
+4. <!--
+5. var myVar = "global"; // Declare a global variable
+6. function checkscope( ) {
+7. }
+8. //-->
+9. </script> 
+10. </body>
+11. </html>
