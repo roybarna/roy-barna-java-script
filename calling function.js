30e9b9808ad03a22b051ece8628971eb0450dd6b
roybarna <117443960+roybarna@users.noreply.github.com>
2023-03-24 16:05:03 +0530

    Add files via upload

---
+++ calling function.js
@@ -0,0 +1,16 @@
+1. <html>
+2. <head> 
+3. <script type = "text/javascript">
+4. function sayHello() {
+5. document.write ("Hello there!");
+6. }
+7. </script>
+8. </head>
+9. <body>
+10. <p>Click the following button to call the function</p> 
+11. <form>
+12. <input type = "button" onclick = "sayHello()" value = "Say Hello">
+13. </form> 
+14. <p>Use different text in write method and then try...</p>
+15. </body>
+16. </html>
