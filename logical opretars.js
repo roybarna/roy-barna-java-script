fa9a7b2e82cd33063d54746de7478a624f488a36
roybarna <117443960+roybarna@users.noreply.github.com>
2023-03-24 15:48:22 +0530

    Add files via upload

---
+++ logical opretars.js
@@ -0,0 +1,24 @@
+1. <html>
+2. <body> 
+3. <script type = "text/javascript">
+4. <!--
+5. var a = true;
+6. var b = false;
+7. var linebreak = "<br />";
+8. document.write("(a && b) => ");
+9. result = (a && b);
+10. document.write(result);
+11. document.write(linebreak);
+12. document.write("(a || b) => ");
+13. result = (a || b);
+14. document.write(result);
+15. document.write(linebreak);
+16. document.write("!(a && b) => ");
+17. result = (!(a && b));
+18. document.write(result);
+19. document.write(linebreak);
+20. //-->
+21. </script> 
+22. <p>Set the variables to different values and different operators and then try...</p>
+23. </body>
+24. </html>
