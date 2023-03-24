54fa2e3fe49902e7b96dd2c1a978d0a6a899493e
roybarna <117443960+roybarna@users.noreply.github.com>
2023-03-24 15:35:50 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,15 @@
+1. <html>
+2. <head>
+3. <script type = "text/javascript">
+4. function sayHello() {
+5. alert("Hello World")
+6. }
+7. </script>
+8. </head>
+9. <body>
+10. <script type = "text/javascript">
+11. document.write("Hello World")
+12. </script>
+13. <input type = "button" onclick = "sayHello()" value = "Say Hello" />
+14. </body>
+15. </html>
