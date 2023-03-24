11a897f8e3c9dd525d43b70781bb8a74bc888f4c
roybarna <117443960+roybarna@users.noreply.github.com>
2023-03-24 15:51:46 +0530

    Add files via upload

---
+++ js dialoge box.js
@@ -0,0 +1,18 @@
+1. <html>
+2. <head> 
+3. <script type = "text/javascript">
+4. <!--
+5. function Warn() {
+6. alert ("This is a warning message!");
+7. document.write ("This is a warning message!");
+8. }
+9. //-->
+10. </script> 
+11. </head>
+12. <body>
+13. <p>Click the following button to see the result: </p> 
+14. <form>
+15. <input type = "button" value = "Click Me" onclick = "Warn();" />
+16. </form> 
+17. </body>
+18. </html>
